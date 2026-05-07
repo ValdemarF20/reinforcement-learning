82333b2ace03b98861d0562fe61a533dfe766ea0
Tue Herlau <tuhe@dtu.dk>
2026-05-07 22:39:22 +0200

    Updated examples for week 13

--- irlc/lectures/lec13/lecture_13_Q_open.py
+++ irlc/lectures/lec13/lecture_13_Q_open.py
@@ -1,5 +1,5 @@
 # This file may not be shared/redistributed without permission. Please read copyright notice in the git repo. If this file contains other copyright notices disregard this text.
-from irlc.lectures.lec11.lecture_10_sarsa_open import open_play
+from irlc.lectures.lec10.lecture_10_sarsa_open import open_play
 from irlc.ex10.q_agent import QAgent
 
 if __name__ == "__main__":
